d633a16c90773573069217907a51a625605dcbf5
Abimbola Idowu <abimbola2010@gmail.com>
2015-01-12 15:12:29 +0100

    created ATM model

---
+++ app/models/atms.server.model.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+
+var atmSchema = new Schema({
+  bank_name: {
+    type: String,
+    required: 'Please enter a bank name',
+  },
+  address: {
+    type: String,
+    address: 'Please enter the address of the ATM',
+    trim: true
+  },
+  state: {
+    type: String,
+    required: 'Please enter a state in Nigeria'
+  },
+  coords: {
+    type: [Number], 
+    index: '2dsphere',
+    required: 'Input the longitude and latitude of the ATM'
+  },
+  createdOn: {
+    type: Date,
+    "default": Date.now
+  }
+});
+
+mongoose.model('ATM', atmSchema);
