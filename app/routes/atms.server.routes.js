f069ed100c88e755990a60c6aab2b9bfb3b8bd5d
Abimbola Idowu <abimbola2010@gmail.com>
2015-01-14 06:53:39 +0100

    editied route to factor out 'atms'

--- app/routes/atms.server.routes.js
+++ app/routes/atms.server.routes.js
@@ -4,14 +4,14 @@ module.exports = function(app) {
   var express = require('express');
   var atms = require('../../app/controllers/atms.server.controller');
   var router = express.Router();
-  app.use('/api/v2', router);
+  app.use('/api/v2/atms', router);
   // router.use();
-    router.route('/atms/search')
+    router.route('/search')
       .get(atms.queryATM);
-    router.route('/atms')
+    router.route('/')
       .get(atms.ATMByDistance)
       .post(atms.createATM);
-    router.route('/atms/:id')
+    router.route('/:id')
       .get(atms.getOneATM)
       .post(atms.updateATM)
       .delete(atms.deleteATM);
